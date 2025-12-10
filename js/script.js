3f11ea89ee36fa10e81beab37f7686aac1acfde7
Slavi <suravides@gmail.com>
2025-12-10 19:53:33 +0700

    methodsOfStringsAndNumbers

--- js/script.js
+++ js/script.js
@@ -1,28 +1,55 @@
 'use strict';
 
-const usdCurr = 28;
-const discount = 0.9;
+// const usdCurr = 28;
+// const discount = 0.9;
 
-function convert(amount, curr) {
-    return curr * amount;
-}
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
 
-function promotion(result) {
-    console.log(result * discount)
-}
+// function promotion(result) {
+//     console.log(result * discount)
+// }
 
-const res = convert(500, usdCurr);
-promotion(res);
+// const res = convert(500, usdCurr);
+// promotion(res);
 
-function test() {
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-        if (i === 3) return
-    }
-    console.log('Done');
-}
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return
+//     }
+//     console.log('Done');
+// }
 
-test();
+// test();
 
-function doNothing() { };
-console.log(doNothing() === undefined);
+// function doNothing() { };
+// console.log(doNothing() === undefined);
+
+
+const str = 'test';
+// const arr = [1, 2, 4];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
