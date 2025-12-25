a54759a17a36005c93784013274655afdcefd322
Slavi <suravides@gmail.com>
2025-12-25 17:02:24 +0700

    ObjectsStudy

--- js/script.js
+++ js/script.js
@@ -1,182 +1,38 @@
 'use strict';
 
-// const usdCurr = 28;
-// const discount = 0.9;
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log("Test");
+    }
+};
 
-// function convert(amount, curr) {
-//     return curr * amount;
-// }
+console.log(Object.keys(options).length);
 
-// function promotion(result) {
-//     console.log(result * discount)
-// }
+// console.log(options.name);
 
-// const res = convert(500, usdCurr);
-// promotion(res);
+// delete options.name;
 
-// function test() {
-//     for (let i = 0; i < 5; i++) {
-//         console.log(i);
-//         if (i === 3) return
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter ++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter ++;
 //     }
-//     console.log('Done');
-// }
+// };
 
-// test();
-
-// function doNothing() { };
-// console.log(doNothing() === undefined);
-
-
-const str = 'test';
-// const arr = [1, 2, 4];
-
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
-
-const fruit = 'Some fruit';
-
-console.log(fruit.indexOf('fruit'));
-
-const logg = 'Hello world';
-
-// console.log(logg.slice(6, 11));
-
-// console.log(logg.substring(6, 11));
-
-console.log(logg.substr(6, 5));
-
-const num = 12.2;
-console.log(Math.round(num));
-
-const test = '12.2px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
-
-// продвинутые задачи на использование функций
-
-function calculateVolumeAndArea(length) {
-    if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
-        return "При вычислении произошла ошибка";
-    }
-
-    let volume = 0;
-    area = 0;
-
-    volume = length * length * length;
-    area = 6 * (length * length);
-
-    return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
-}
-
-calculateVolumeAndArea(5);
-
-function getCoupeNubmer(seatNumber) {
-    if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
-        return "Ошибка. Проверьте правильность введенного номера места";
-    }
-
-    if (seatNumber === 0 || seatNumber > 36) {
-        return "Таких мест в вагоне не существует";
-    }
-
-    return Math.ceil(seatNumber / 4);
-}
-
-
-function getTimeFromMinutes(minutesTotal) {
-    if (typeof (minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
-        return "Ошибка, проверьте данные";
-    }
-
-    const hours = Math.floor(minutesTotal / 60);
-    const minutes = minutesTotal % 60;
-
-    let hoursStr = '';
-
-    switch (hours) {
-        case 0:
-            hoursStr = 'часов';
-            break;
-        case 1:
-            hoursStr = 'час';
-            break;
-        case 2:
-        case 3:
-        case 4:
-            hoursStr = 'часа';
-        default:
-            hoursStr = 'часов';
-    }
-
-    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
-}
-
-getTimeFromMinutes(180);
-
-function findMaxNumber(a, b, c, d) {
-    if (typeof (a) !== 'number' ||
-        typeof (b) !== 'number' ||
-        typeof (c) !== 'number' ||
-        typeof (d) !== 'number') {
-        return 0;
-    } else {
-        return Math.max(a, b, c, d);
-    }
-}
-
-findMaxNumber(1, 5, 6.6, 10.5);
-findMaxNumber(1, 5, '6', '10');
-
-function fib(num) {
-    if (typeof (num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-        return "";
-    }
-
-    let result = '';
-    let first = 0;
-    let second = 1;
-
-    for (let i = 0; i < num; i++) {
-        if (i + 1 === num) {
-            result += `${first}`;
-        } else {
-            result += `${first} `;
-        }
-
-        let third = first + second;
-        first = second;
-        second = third;
-    }
-
-    return result;
-}
-
-fib(5)
-
-
-function first() {
-    //Do something
-    setTimeout(function () {
-        console.log(1);
-    }, 500);
-}
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done);
+// console.log(counter);
