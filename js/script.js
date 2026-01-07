a52297340cc79dca615aa3ec755e6a8a548a8887
Slavi <suravides@gmail.com>
2026-01-07 19:18:51 +0700

    practiceMassives

--- js/script.js
+++ js/script.js
@@ -1,97 +1,165 @@
 'use strict';
 
-// let a = 5,
-//     b = a;
+// // let a = 5,
+// //     b = a;
 
-// b = b + 5;
+// // b = b + 5;
 
-// console.log(b);
-// console.log(a);
+// // console.log(b);
+// // console.log(a);
 
-// const obj = {
-//     a: 5,
-//     b: 1
+// // const obj = {
+// //     a: 5,
+// //     b: 1
+// // };
+
+// // const copy = obj; //Ссылку (не копируется объект а кладётся ссылка на него)
+
+// // copy.a = 10; //Модифицируя копия мы модифицируем сам объект
+
+// // console.log(copy);
+// // console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
 // };
 
-// const copy = obj; //Ссылку (не копируется объект а кладётся ссылка на него)
+// const newNumbers = copy(numbers);
 
-// copy.a = 10; //Модифицируя копия мы модифицируем сам объект
+// newNumbers.a = 10;
 
-// console.log(copy);
-// console.log(obj);
+// console.log(newNumbers);
+// console.log(numbers);
 
-function copy(mainObj) {
-    let objCopy = {};
+// const add = {
+//     d: 17,
+//     e: 20
+// };
 
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
+// console.log(Object.assign(numbers, add));
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'asdadwawd';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'livejournal', 'blogger'],
+//     internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = { ...q };
+// console.log(newObj);
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function (plan) {
+        const { age } = plan;
+        const { languages } = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
+
+        languages.forEach(function (lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+
+        return str;
     }
+};
 
-    return objCopy;
+function showExperience(plan) {
+    const { exp } = plan.skills;
+    return exp;
 }
 
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+showExperience(personalPlanPeter);
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const { programmingLangs } = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
     }
-};
 
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-
-console.log(newNumbers);
-console.log(numbers);
-
-const add = {
-    d: 17,
-    e: 20
-};
-
-console.log(Object.assign(numbers, add));
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'asdadwawd';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+    return str;
 }
 
-const num = [2, 5, 7];
+showProgrammingLangs(personalPlanPeter);
 
-log(...num);
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
-const array = ['a', 'b'];
+function showFamily(arr) {
+    if (arr.length === 0) {
+        return 'Семья пуста';
+    }
 
-const newAarray = [...array];
+    return `Семья состоит из: ${arr.join(' ')}`;
 
-const q = {
-    one: 1,
-    two: 2
-};
 
-const newObj = { ...q };
-console.log(newObj);
+}
+
+showFamily(family);
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(city => {
+        console.log(city.toLowerCase());
+    });
+
+}
+
+standardizeStrings(favoriteCities);
