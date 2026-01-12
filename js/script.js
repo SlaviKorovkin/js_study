8d7bde53a39fcb7042b432f58e764a46db1b8890
Slavi <suravides@gmail.com>
2026-01-12 16:41:36 +0700

    practiceMassives.2

--- js/script.js
+++ js/script.js
@@ -163,3 +163,20 @@ function standardizeStrings(arr) {
 }
 
 standardizeStrings(favoriteCities);
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof (str) !== 'string') {
+        return 'Ошибка!';
+    }
+
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+
+    return newStr;
+}
+
+reverse(someString);
